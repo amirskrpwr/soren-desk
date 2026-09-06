6a50181be5fc3146d916aad9c4c6d7fe80b5a60d
amirskrpwr <amhskrpwr@gmail.com>
2026-09-06 12:22:47 +0330

    Update project branding to SorenDesk
    This commit changes the project name and associated branding from RustDesk to SorenDesk across various files, including:
    - Updated authors and descriptions in `Cargo.toml`
    - Modified application labels in `AndroidManifest.xml` and `Info.plist`
    - Changed submodule URL in `.gitmodules`
    - Updated Flutter package name and description in `pubspec.yaml`
    - Adjusted notification titles and messages in service files
    - Replaced icons and logos in the assets directory
    
    Additionally, color constants in the theme have been updated to reflect the new branding. This comprehensive update ensures a consistent identity for the application.

--- flutter/lib/common.dart
+++ flutter/lib/common.dart
@@ -249,16 +249,16 @@ class MyTheme {
   MyTheme._();
 
   static const Color grayBg = Color(0xFFEFEFF2);
-  static const Color accent = Color(0xFF0071FF);
-  static const Color accent50 = Color(0x770071FF);
-  static const Color accent80 = Color(0xAA0071FF);
+  static const Color accent = Color(0xFF0086D7);
+  static const Color accent50 = Color(0x770086D7);
+  static const Color accent80 = Color(0xAA0086D7);
   static const Color canvasColor = Color(0xFF212121);
   static const Color border = Color(0xFFCCCCCC);
-  static const Color idColor = Color(0xFF00B6F0);
+  static const Color idColor = Color(0xFF0086D7);
   static const Color darkGray = Color.fromARGB(255, 148, 148, 148);
   static const Color cmIdColor = Color(0xFF21790B);
   static const Color dark = Colors.black87;
-  static const Color button = Color(0xFF2C8CFF);
+  static const Color button = Color(0xFF0086D7);
   static const Color hoverBorder = Color(0xFF999999);
 
   // ListTile
@@ -452,7 +452,7 @@ class MyTheme {
         style:
             MenuStyle(backgroundColor: MaterialStatePropertyAll(Colors.white))),
     colorScheme: ColorScheme.light(
-        primary: Colors.blue, secondary: accent, background: grayBg),
+        primary: accent, secondary: accent, background: grayBg),
     popupMenuTheme: PopupMenuThemeData(
         color: Colors.white,
         shape: RoundedRectangleBorder(
@@ -560,7 +560,7 @@ class MyTheme {
         style: MenuStyle(
             backgroundColor: MaterialStatePropertyAll(Color(0xFF121212)))),
     colorScheme: ColorScheme.dark(
-      primary: Colors.blue,
+      primary: accent,
       secondary: accent,
       background: Color(0xFF24252B),
     ),
@@ -1321,7 +1321,7 @@ Color? _msgboxColor(String type) {
   if (type.contains("error") || type == "re-input-password") {
     return Color(0xFFE04F5F);
   }
-  return Color(0xFF2C8CFF);
+  return Color(0xFF0086D7);
 }
 
 Widget msgboxIcon(String type) {
--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -32,7 +32,7 @@ class DesktopHomePage extends StatefulWidget {
   State<DesktopHomePage> createState() => _DesktopHomePageState();
 }
 
-const borderColor = Color(0xFF2F65BA);
+const borderColor = Color(0xFF0086D7);
 
 class _DesktopHomePageState extends State<DesktopHomePage>
     with AutomaticKeepAliveClientMixin, WidgetsBindingObserver {
